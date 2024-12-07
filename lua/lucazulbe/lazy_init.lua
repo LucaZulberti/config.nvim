d83f65e3e0b7cb1ebb814d2f669af999343713f5
Luca Zulberti <zulberti.luca@gmail.com>
2024-12-08 00:52:16 +0100

    ♻️ Separate VSCode compatible and non- plugins

--- lua/lucazulbe/lazy_init.lua
+++ lua/lucazulbe/lazy_init.lua
@@ -12,6 +12,10 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = "lucazulbe.lazy",
+    spec = {
+        { import = "lucazulbe.lazy_notvscode", cond = (function() return not vim.g.vscode end) },
+    	{ import = "lucazulbe.lazy_always",    cond = true },
+	    { import = "lucazulbe.lazy_vscode",    cond = (function() return vim.g.vscode end) },
+    },
     change_detection = { notify = false }
 })
--- lua/lucazulbe/lazy_notvscode/colors.lua
+++ lua/lucazulbe/lazy_notvscode/colors.lua
@@ -6,6 +6,10 @@ function ColorMyPencils(color)
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
+vim.api.nvim_create_user_command("ColorMyPencils", function(opts)
+    ColorMyPencils(opts.args)
+end, { nargs = "?" })
+
 return {
 
     {
