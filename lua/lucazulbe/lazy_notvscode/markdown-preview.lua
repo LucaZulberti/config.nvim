6b40e217fd85954240d7bb7fdc861cd6e873e642
Luca Zulberti <zulberti.luca@gmail.com>
2024-12-09 22:00:14 +0100

    :technologist: Add vim-tmux-navigator and markdown-preview plugins

---
+++ lua/lucazulbe/lazy_notvscode/markdown-preview.lua
@@ -0,0 +1,12 @@
+return {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+        vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+    config = function()
+        vim.api.nvim_set_keymap('n', '<leader>p', '<cmd>MarkdownPreviewToggle<CR>', { noremap = true, silent = true, desc = "Toggle markdown preview" })
+    end,
+}
