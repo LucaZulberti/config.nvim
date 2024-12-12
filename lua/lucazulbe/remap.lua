5e3a655a7e4bdbb31931081f7de23a3229d9795b
Luca Zulberti <zulberti.luca@gmail.com>
2024-12-12 11:04:18 +0100

    :technologist: Improve remap keys and add descriptions

--- lua/lucazulbe/remap.lua
+++ lua/lucazulbe/remap.lua
@@ -1,96 +1,50 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Project view (directory where I launched nvim)
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- Move text
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join next line" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up" })
 
--- Move line while in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- Move cursor
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Go down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Go up and center" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Go next, center, and unfold" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Go previous, center, and unfold" })
 
--- Bring next line at the end of this one
-vim.keymap.set("n", "J", "mzJ`z")
+-- System clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank to system clipboard" })
 
--- Go down/up and center
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Registers
+vim.keymap.set("n", "Q", "<nop>", { desc = "Disable replay last recorded register" })
 
--- Go next/previous and center with unfold
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+-- Void register
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Replace into void register" })
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete into void register" })
+vim.keymap.set({ "n", "v" }, "<leader>c", [["_c]], { desc = "Change into void register" })
+vim.keymap.set({ "n", "v" }, "<leader>di", [["_di]], { desc = "Delete into void register inside" })
+vim.keymap.set({ "n", "v" }, "<leader>ci", [["_ci]], { desc = "Change into void register inside" })
 
+-- Replace
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+    { desc = "Replace word under cursor" })
 
--- Restart LSP
-vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
-
--- I'm not using vim-with-me plugin
---[[
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
---]]
-
--- Replace without overriding copied content
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- Yank into system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
--- Delete into void register
-vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
-
--- Esc is annoying, Ctrl+c is better, unify meaning
-vim.keymap.set("i", "<C-c>", "<Esc>")
-
--- I don't want to use it
-vim.keymap.set("n", "Q", "<nop>")
-
--- TODO: Explore tmux
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+-- Project
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open project view" })
 
 -- QuickFix navigation shortcuts
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz", { desc = "Go to next quickfix item" })
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz", { desc = "Go to previous quickfix item" })
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz", { desc = "Go to next location list item" })
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "Go to previous location list item" })
 
--- Start replacing word I'm on
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- Work on current file
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make the current file executable" })
+vim.keymap.set("n", "<leader><leader>", "<cmd>so<CR>", { desc = "Source the current file" });
 
--- Make the current file executable
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
-vim.keymap.set(
-    "n",
-    "<leader>ee",
-    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
-)
-
-vim.keymap.set(
-    "n",
-    "<leader>ea",
-    "oassert.NoError(err, \"\")<Esc>F\";a"
-)
-
-vim.keymap.set(
-    "n",
-    "<leader>ef",
-    "oif err != nil {<CR>}<Esc>Olog.Fatalf(\"error: %s\\n\", err.Error())<Esc>jj"
-)
-
-vim.keymap.set(
-    "n",
-    "<leader>el",
-    "oif err != nil {<CR>}<Esc>O.logger.Error(\"error\", \"error\", err)<Esc>F.;i"
-)
-
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
-
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
+-- Misc
+vim.keymap.set("n", "<leader>vvv", "<cmd>e ~/.config/nvim<CR>", { desc = "Edit Neovim config" });
+vim.keymap.set("i", "<C-c>", "<Esc>", { desc = "Escape in insert mode" })
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ~/.config/tmuxp/tmuxp-sessionizer<CR>",
+    { desc = "Open tmuxp sessionizer" })
