96e0035f5776b129b931cfeff8edc447a811f665
Luca Zulberti <zulberti.luca@gmail.com>
2024-12-24 19:21:02 +0100

    :lipstick: Update my colors

--- lua/lucazulbe/lazy_notvscode/colors.lua
+++ lua/lucazulbe/lazy_notvscode/colors.lua
@@ -1,9 +1,6 @@
 function ColorMyPencils(color)
     color = color or "rose-pine-moon"
     vim.cmd.colorscheme(color)
-
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
@@ -13,9 +10,8 @@ return {
 
     config = function()
         require("rose-pine").setup({
-            variant = "moon", -- auto, main, moon, or dawn
-            dark_variant = "moon", -- main, moon, or dawn
-            dim_inactive_windows = false,
+            variant = "moon",
+            dim_inactive_windows = true,
             extend_background_behind_borders = true,
 
             enable = {
