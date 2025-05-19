ae678295acd711dae684aea669785cb39de14304
Luca Zulberti <zulberti.luca@gmail.com>
2025-05-19 23:11:53 +0200

    :technologist: Update LSP configuration

--- lua/lucazulbe/lazy_notvscode/lsp.lua
+++ lua/lucazulbe/lazy_notvscode/lsp.lua
@@ -81,23 +81,6 @@ return {
                         on_attach = on_attach, -- Attach the local on_attach function
                     }
                 end,
-
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                        on_attach = on_attach, -- Attach the local on_attach function
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
-                end,
                 ["lua_ls"] = function()
                     local lspconfig = require("lspconfig")
                     lspconfig.lua_ls.setup {
