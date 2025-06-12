5229c3506659ed61687a899909d041ba9994ed28
Luca Zulberti <zulberti.luca@gmail.com>
2025-06-12 15:05:00 +0200

    :technologist: telescope: Follow symbolic links

--- lua/lucazulbe/lazy_notvscode/telescope.lua
+++ lua/lucazulbe/lazy_notvscode/telescope.lua
@@ -9,21 +9,29 @@ return {
 
     config = function()
         require('telescope').setup({})
-
         local builtin = require('telescope.builtin')
 
         -- Find
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Project find files" })
+        vim.keymap.set('n', '<leader>pf', function()
+            builtin.find_files({
+                follow = true
+            })
+        end, { desc = "Project find files" })
+
         vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Git find files" })
 
         -- Search
         vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+            builtin.grep_string({
+                search = vim.fn.input("Grep > ")
+            })
         end, { desc = "Project search" })
+
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
         end, { desc = "Project search word" })
+
         vim.keymap.set('n', '<leader>pWs', function()
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
