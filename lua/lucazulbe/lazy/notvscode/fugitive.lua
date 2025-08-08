60e611d4ef891e99056e97f76b368749df166def
Luca Zulberti <zulberti.luca@gmail.com>
2025-08-08 22:43:17 +0200

    :children_crossing: Remove useless custom fugitive remaps

--- lua/lucazulbe/lazy/notvscode/fugitive.lua
+++ lua/lucazulbe/lazy/notvscode/fugitive.lua
@@ -13,23 +13,7 @@ return {
                 if vim.bo.ft ~= "fugitive" then
                     return
                 end
-
-                local bufnr = vim.api.nvim_get_current_buf()
-                local opts = { buffer = bufnr, remap = false }
-
-                -- Common operations
-                vim.keymap.set("n", "<leader>p", function()
-                    vim.cmd.Git('push')
-                end, vim.tbl_extend("force", opts, { desc = "Git push" }))
-                vim.keymap.set("n", "<leader>P", function()
-                    vim.cmd.Git({ 'pull', '--rebase' })
-                end, vim.tbl_extend("force", opts, { desc = "Git pull with rebase" }))
-                vim.keymap.set("n", "<leader>t", ":Git push -u origin ",
-                    vim.tbl_extend("force", opts, { desc = "Git push and track origin" }))
             end,
         })
-
-        vim.keymap.set("n", "gf", "<cmd>diffget //2<CR>", { desc = "Get right diff" })
-        vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>", { desc = "Get left diff" })
     end
 }
