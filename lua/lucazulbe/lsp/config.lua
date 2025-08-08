91eabf928ca7c8a5b56573b19867e574c1f50335
Luca Zulberti <zulberti.luca@gmail.com>
2025-08-08 21:55:45 +0200

    :rotating_light: Use <leader> instead of <space>

--- lua/lucazulbe/lsp/config.lua
+++ lua/lucazulbe/lsp/config.lua
@@ -19,44 +19,44 @@ M.on_attach = function(client, bufnr)
 
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, vim.tbl_extend('force', opts,
         { desc = "Show hover info" }))
-    vim.keymap.set('n', '<space>lK', vim.lsp.buf.signature_help, vim.tbl_extend('force', opts,
+    vim.keymap.set('n', '<leader>lK', vim.lsp.buf.signature_help, vim.tbl_extend('force', opts,
         { desc = "Show signature help" }))
 
-    vim.keymap.set('n', '<space>ld', vim.lsp.buf.definition, vim.tbl_extend('force', opts,
+    vim.keymap.set('n', '<leader>ld', vim.lsp.buf.definition, vim.tbl_extend('force', opts,
         { desc = "Jump to definition" }))
-    vim.keymap.set('n', '<space>lD', vim.lsp.buf.declaration, vim.tbl_extend('force', opts,
+    vim.keymap.set('n', '<leader>lD', vim.lsp.buf.declaration, vim.tbl_extend('force', opts,
         { desc = "Jump to declaration" }))
-    vim.keymap.set('n', '<space>lv', '<cmd>vsplit | lua vim.lsp.buf.definition()<CR>',
+    vim.keymap.set('n', '<leader>lv', '<cmd>vsplit | lua vim.lsp.buf.definition()<CR>',
         vim.tbl_extend('force', opts,
             { desc = "Open definition in vertical split" }))
-    vim.keymap.set('n', '<space>lV', '<cmd>vsplit | lua vim.lsp.buf.declaration()<CR>',
+    vim.keymap.set('n', '<leader>lV', '<cmd>vsplit | lua vim.lsp.buf.declaration()<CR>',
         vim.tbl_extend('force', opts,
             { desc = "Open declaration in vertical split" }))
-    vim.keymap.set('n', '<space>lh', '<cmd>split | lua vim.lsp.buf.definition()<CR>',
+    vim.keymap.set('n', '<leader>lh', '<cmd>split | lua vim.lsp.buf.definition()<CR>',
         vim.tbl_extend('force', opts,
             { desc = "Open definition in horizzontal split" }))
-    vim.keymap.set('n', '<space>lH', '<cmd>split | lua vim.lsp.buf.declaration()<CR>',
+    vim.keymap.set('n', '<leader>lH', '<cmd>split | lua vim.lsp.buf.declaration()<CR>',
         vim.tbl_extend('force', opts,
             { desc = "Open declaration in horizzontal split" }))
-    vim.keymap.set('n', '<space>lt', '<cmd>tab split | lua vim.lsp.buf.definition()<CR>',
+    vim.keymap.set('n', '<leader>lt', '<cmd>tab split | lua vim.lsp.buf.definition()<CR>',
         vim.tbl_extend('force', opts,
             { desc = "Open definition in a new tab" }))
-    vim.keymap.set('n', '<space>lT', '<cmd>tab split | lua vim.lsp.buf.declaration()<CR>',
+    vim.keymap.set('n', '<leader>lT', '<cmd>tab split | lua vim.lsp.buf.declaration()<CR>',
         vim.tbl_extend('force', opts,
             { desc = "Open declaration in a new tab" }))
 
-    vim.keymap.set('n', '<space>lrf', vim.lsp.buf.references, vim.tbl_extend('force', opts,
+    vim.keymap.set('n', '<leader>lrf', vim.lsp.buf.references, vim.tbl_extend('force', opts,
         { desc = "List references of current symbol" }))
-    vim.keymap.set('n', '<space>lrn', vim.lsp.buf.rename, vim.tbl_extend('force', opts,
+    vim.keymap.set('n', '<leader>lrn', vim.lsp.buf.rename, vim.tbl_extend('force', opts,
         { desc = "Rename current symbol" }))
-    vim.keymap.set('n', '<space>lca', vim.lsp.buf.code_action, vim.tbl_extend('force', opts,
+    vim.keymap.set('n', '<leader>lca', vim.lsp.buf.code_action, vim.tbl_extend('force', opts,
         { desc = "List code actions" }))
 
-    vim.keymap.set('n', '<space>lwa', vim.lsp.buf.add_workspace_folder, vim.tbl_extend('force', opts,
+    vim.keymap.set('n', '<leader>lwa', vim.lsp.buf.add_workspace_folder, vim.tbl_extend('force', opts,
         { desc = "Add working directory or current folder to workspace list" }))
-    vim.keymap.set('n', '<space>lwr', vim.lsp.buf.remove_workspace_folder, vim.tbl_extend('force', opts,
+    vim.keymap.set('n', '<leader>lwr', vim.lsp.buf.remove_workspace_folder, vim.tbl_extend('force', opts,
         { desc = "Remove working directory or current folder to workspace list" }))
-    vim.keymap.set('n', '<space>lwl', function()
+    vim.keymap.set('n', '<leader>lwl', function()
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, vim.tbl_extend('force', opts,
         { desc = "List workspace folders" }))
