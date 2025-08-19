9d25d170ec7a3ce71b4d87568cb7752549d6c0d2
Luca Zulberti <zulberti.luca@gmail.com>
2025-08-19 11:19:48 +0200

    [UPDATE] lsp: Define keymaps only when LSPs attach to buffers

--- lua/lucazulbe/lsp.lua
+++ lua/lucazulbe/lsp.lua
@@ -1,28 +1,34 @@
 -- Shortcut
 local map = vim.keymap.set
 
-
--- Add LSP commands
----
-
--- Format with conform in pack.lua
--- map('n', '<leader>lf', vim.lsp.buf.format, { desc = "[LSP] Format current buffer" })
-
-map("n", '<leader>lr', function()
-    local bufnr = vim.api.nvim_get_current_buf()
-    local clients = vim.lsp.get_clients({ bufnr = bufnr })
-
-    for _, client in ipairs(clients) do
-        vim.lsp.stop_client(client.id)
-    end
-
-    vim.defer_fn(function()
-        vim.cmd("edit") -- re-edit buffer to retrigger LSP attachment
-    end, 100)
-end, { desc = "[LSP] Restart for current buffer" })
-
 -- Configure each LSP
----
+-- ------------------
+
+vim.lsp.config("*", {
+    on_attach = function()
+        -- Add LSP commands
+        -- ----------------
+
+        -- Format with conform in pack.lua
+        -- map('n', '<leader>lf', vim.lsp.buf.format, { desc = "[LSP] Format current buffer" })
+
+        map('n', '<leader>lr', function()
+            local bufnr = vim.api.nvim_get_current_buf()
+            local clients = vim.lsp.get_clients({ bufnr = bufnr })
+
+            for _, client in ipairs(clients) do
+                vim.lsp.stop_client(client.id)
+            end
+
+            vim.defer_fn(function()
+                vim.cmd("edit") -- re-edit buffer to retrigger LSP attachment
+            end, 100)
+        end, { desc = "[LSP] Restart for current buffer" })
+
+        map('n', 'K', function() vim.lsp.buf.hover() end,
+            { noremap = true, silent = true, desc = "[LSP] Open information in hover buffer" })
+    end
+})
 
 vim.lsp.config("tinymist", {
     settings = {
