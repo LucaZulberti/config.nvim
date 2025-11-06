0a9533b4b5f06c86461832f9abcce97e392a7255
Luca Zulberti <zulberti.luca@gmail.com>
2025-11-06 11:07:01 +0100

    [UPDATE] lsp: Add PyLsp with Ruff

--- lua/lucazulbe/lsp.lua
+++ lua/lucazulbe/lsp.lua
@@ -45,3 +45,63 @@ vim.lsp.config("remark_ls", {
 
     }
 })
+
+vim.lsp.config("pylsp", {
+    settings = {
+        pylsp = {
+            plugins = {
+                -- Disable default formatters/linters
+                pycodestyle = {
+                    enabled = false
+                },
+                mccabe = {
+                    enabled = false
+                },
+                pyflakes = {
+                    enabled = false
+                },
+                autopep8 = {
+                    enabled = false
+                },
+                yapf = {
+                    enabled = false
+                },
+
+                -- Enable Ruff
+                ruff = {
+                    enabled = true,
+                    formatEnabled = true, -- Enable formatting
+                    lineLength = 120,
+                },
+            }
+        }
+    }
+})
+
+-- For PyLSP using Ruff
+-----------------------
+
+-- Check if python-lsp-server and the python-lsp-ruff plugin are installed
+local function ensure_pylsp_plugins()
+    local pylsp_path = vim.fn.stdpath("data") .. "/mason/packages/python-lsp-server"
+    local venv_pip = pylsp_path .. "/venv/bin/pip"
+
+    -- Check if pylsp is installed
+    if vim.fn.isdirectory(pylsp_path) == 1 then
+        -- Install python-lsp-ruff
+        vim.fn.system(venv_pip .. " install python-lsp-ruff")
+    end
+end
+
+-- Run after Mason is loaded
+vim.api.nvim_create_autocmd("User", {
+    pattern = "MasonToolsUpdateCompleted",
+    callback = ensure_pylsp_plugins,
+})
+
+-- Also run on VimEnter in case it's already installed
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        vim.defer_fn(ensure_pylsp_plugins, 100)
+    end,
+})
