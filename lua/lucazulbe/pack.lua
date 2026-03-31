18d6843062a3eaefe485ef39eb9b546b5c2b6bf7
Luca Zulberti <zulberti.luca@gmail.com>
2026-03-31 16:50:45 +0200

    [UPDATE] pack: Use github http helper, use latest nvim-treesitter
    Required tree-sitter-cli installed on host.

--- lua/lucazulbe/pack.lua
+++ lua/lucazulbe/pack.lua
@@ -1,64 +1,68 @@
 local funcs = require("lucazulbe.functions")
 local rmap = funcs.rmap
 
+local gh = function(x) return 'https://github.com/' .. x end
+
 -- Plugin installation
 -- -------------------
 
 vim.pack.add({
     -- Colorschemes
-    { src = "https://github.com/rose-pine/neovim" },
-    { src = "https://github.com/catppuccin/nvim" },
+    { src = gh("rose-pine/neovim") },
+    { src = gh("catppuccin/nvim") },
 
     -- Miscellaneous
-    { src = "https://github.com/Eandrju/cellular-automaton.nvim" },
-    { src = "https://github.com/tpope/vim-sleuth" },
-    { src = "https://github.com/tpope/vim-fugitive" },
-    { src = "https://github.com/mhinz/vim-signify" },
-    { src = "https://github.com/danymat/neogen" },
-    { src = "https://github.com/stevearc/oil.nvim" },
-    { src = "https://github.com/christoomey/vim-tmux-navigator" },
+    { src = gh("Eandrju/cellular-automaton.nvim") },
+    { src = gh("tpope/vim-sleuth") },
+    { src = gh("tpope/vim-fugitive") },
+    { src = gh("mhinz/vim-signify") },
+    { src = gh("stevearc/oil.nvim") },
+    { src = gh("christoomey/vim-tmux-navigator") },
 
     -- mini.nvim modules
-    { src = "https://github.com/echasnovski/mini.ai" },
-    { src = "https://github.com/echasnovski/mini.operators" },
-    { src = "https://github.com/echasnovski/mini.surround" },
-    { src = "https://github.com/echasnovski/mini.snippets" },
-    { src = "https://github.com/echasnovski/mini.completion" },
-    { src = "https://github.com/echasnovski/mini.hipatterns" },
-    { src = "https://github.com/echasnovski/mini.move" },
-    { src = "https://github.com/echasnovski/mini.pairs" },
-    { src = "https://github.com/echasnovski/mini.pick" },
-    { src = "https://github.com/echasnovski/mini.files" },
-    { src = "https://github.com/echasnovski/mini.extra" },
-    { src = "https://github.com/echasnovski/mini.icons" },
-    { src = "https://github.com/echasnovski/mini.trailspace" },
+    { src = gh("echasnovski/mini.ai") },
+    { src = gh("echasnovski/mini.operators") },
+    { src = gh("echasnovski/mini.surround") },
+    { src = gh("echasnovski/mini.snippets") },
+    { src = gh("echasnovski/mini.completion") },
+    { src = gh("echasnovski/mini.hipatterns") },
+    { src = gh("echasnovski/mini.move") },
+    { src = gh("echasnovski/mini.pairs") },
+    { src = gh("echasnovski/mini.pick") },
+    { src = gh("echasnovski/mini.files") },
+    { src = gh("echasnovski/mini.extra") },
+    { src = gh("echasnovski/mini.icons") },
+    { src = gh("echasnovski/mini.trailspace") },
 
     -- Treesitter and UI
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-    { src = "https://github.com/j-hui/fidget.nvim" },
+    { src = gh("nvim-treesitter/nvim-treesitter"), version = 'main' },
+    { src = gh("j-hui/fidget.nvim") },
 
-    -- LSP and tooling
-    { src = "https://github.com/neovim/nvim-lspconfig" },
-    { src = "https://github.com/mason-org/mason.nvim" },
-    { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-    { src = "https://github.com/stevearc/conform.nvim" },
+    -- LSP and Formatter
+    { src = gh("neovim/nvim-lspconfig") },
+    { src = gh("mason-org/mason.nvim") },
+    { src = gh("mason-org/mason-lspconfig.nvim") },
+    { src = gh("stevearc/conform.nvim") },
 
     -- Dependencies and build tooling
-    { src = "https://github.com/nvim-lua/plenary.nvim" },
-    { src = "https://github.com/Civitasv/cmake-tools.nvim" },
+    { src = gh("nvim-lua/plenary.nvim") },
+    { src = gh("Civitasv/cmake-tools.nvim") },
+
+    -- Documentation
+    { src = gh("danymat/neogen") },
 
     -- Debugging
-    { src = "https://github.com/mfussenegger/nvim-dap" },
-    { src = "https://github.com/theHamsta/nvim-dap-virtual-text" },
-    { src = "https://github.com/nvim-neotest/nvim-nio" },
-    { src = "https://github.com/rcarriga/nvim-dap-ui" },
+    { src = gh("mfussenegger/nvim-dap") },
+    { src = gh("theHamsta/nvim-dap-virtual-text") },
+    { src = gh("nvim-neotest/nvim-nio") },
+    { src = gh("rcarriga/nvim-dap-ui") },
 
     -- Markup and document preview
-    { src = "https://github.com/OXY2DEV/markview.nvim" },
-    { src = "https://github.com/chomosuke/typst-preview.nvim" },
+    { src = gh("OXY2DEV/markview.nvim") },
+    { src = gh("chomosuke/typst-preview.nvim") },
 
     -- Language-specific tooling
-    { src = "https://github.com/mrcjkb/rustaceanvim" },
+    { src = gh("mrcjkb/rustaceanvim") },
 })
 
 -- Plugin configuration
@@ -70,20 +74,16 @@ require("rose-pine").setup({
 })
 
 require("oil").setup()
-require("neogen").setup()
 
--- Enable syntax-aware highlighting and install a small baseline parser set.
-require("nvim-treesitter.configs").setup({
-    ensure_installed = { "lua", "vim", "bash", "fish", "c", "cpp" },
-    auto_install = true,
-    highlight = { enable = true },
-})
+require("nvim-treesitter").install { "lua", "vim", "bash", "fish", "c", "cpp" }
 
 require("fidget").setup()
 
 -- Set up Mason and bridge it to lspconfig-managed servers.
 require("mason").setup()
 require("mason-lspconfig").setup()
+
+require("neogen").setup()
 
 -- Load local formatter and CMake/DAP integration modules.
 require("lucazulbe.pack.conform")
